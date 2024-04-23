c6bf65c1dbce76371015bae5abc9f44a77b3b953
𐀔𓂃 Yuii! ⋆.˚ <167827215+justyuii@users.noreply.github.com>
2024-04-23 13:46:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
+#            APP CONTROL GLIC ! <<<<<<<<<<<<<<<<<<<<<<<<<,
 
+users = open('users.txt', 'a')
+
+#
+
+def cadastro():
+    useremail = input("Insira o seu email:\n")
+    userpassword = input("Crie uma senha:\n")
+    confirmpassword = input("Confirme a sua senha:\n")
+
+    if useremail in 'users.txt':
+        print('Esse email já está sendo utilizado.')
+    else:
+        if confirmpassword == userpassword:
+            users.write('{}\n'.format(useremail, userpassword))
+            print('Cadastro efetuado com sucesso!')
+        else:
+            print("As senhas não coincidem.")
+
+def login():
+    userlogin = input("Insira o seu email:\n")
+    # INCOMPLETO !!
+
+#
+
+escolhaentrada = int(input("1 - > Cadastro\n2 - > Login\n"))
+if escolhaentrada == 1:
+    cadastro()
+elif escolhaentrada == 2:
+    login()
+else:
+    print("Opção inválida.")
+
+#
