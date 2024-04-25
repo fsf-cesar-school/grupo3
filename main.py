0401a37b8f183bb71a8e2f980aad8b56271d16ca
𐀔𓂃 Yuii! ⋆.˚ <167827215+justyuii@users.noreply.github.com>
2024-04-25 18:26:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,49 @@
-#            APP CONTROL GLIC ! <<<<<<<<<<<<<<<<<<<<<<<<<
+"""              APP CONTROL GLIC !              """
+
+# •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
 users = open('users.txt', 'a')
 
-#
+# •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
 def cadastro():
-    useremail = input("Insira o seu email:\n")
-    userpassword = input("Crie uma senha:\n")
-    confirmpassword = input("Confirme a sua senha:\n")
+    useremail = input("» Insira o seu endereço de email:\n")
+    userpassword = input("» Crie uma senha:\n")
+    confirmpassword = input("» Confirme a sua senha:\n")
 
     if useremail in 'users.txt':
-        print('Esse email já está sendo utilizado.')
+        print('» Esse email já está sendo utilizado.')
     else:
         if confirmpassword == userpassword:
             users.write(f'[{useremail, userpassword}]\n')
-            print('Cadastro efetuado com sucesso!')
+            print('» Cadastro efetuado com sucesso!')
         else:
-            print("As senhas não coincidem.")
+            print("» As senhas não coincidem.")
+
+# •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
 def login():
-    userlogin = input("Insira o seu email:\n")
+    userlogin = input("» Insira o seu endereço de email:\n")
     open('users.txt', 'r')
     if userlogin in 'users.txt':
-        userpwd = input("Insira sua senha")
+        userpwd = input("» Insira sua senha")
         if userpwd in 'users.txt':
-            print("Login efetuado com sucesso.")
+            print("» Login efetuado com sucesso.")
         else:
-            print("Senha incorreta.")
+            print("» Senha incorreta.")
     else:
-        print("Endereço de email não cadastrado.")
+        print("» Endereço de email não cadastrado.")
 
-#
+# •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
-escolhaentrada = int(input("1 - > Cadastro\n2 - > Login\n"))
+# TELA DE INÍCIO - CADASTRO/LOGIN #
+
+escolhaentrada = int(input("» O que deseja fazer?\n1 → Cadastro\n2 → Login\n"))
 if escolhaentrada == 1:
     cadastro()
 elif escolhaentrada == 2:
     login()
 else:
-    print("Opção inválida.")
+    print("» Opção inválida.")
 
-#
+# •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
