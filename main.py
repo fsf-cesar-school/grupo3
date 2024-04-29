a62eb1a57a4e0ee864ca64a649d67add22d73f0c
𐀔𓂃 Yuii! ⋆.˚ <167827215+justyuii@users.noreply.github.com>
2024-04-29 20:57:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,20 +13,40 @@ def cadastro():
 
     with open('users.txt', 'r') as f:
         if useremail in f.read():
-            escolhaemail = int(input('» Esse email já está sendo utilizado.\n» Deseja fazer login?\n1 → Sim\n2 → Não, tentar outro email.'))
-            if escolhaemail == 1:
-                login()
-            elif escolhaemail == 2:
-                cadastro()
-            else:
-                print("» Opção inválida..")
+            print('» Esse email já está sendo utilizado.')
+            return False
         else:
             if confirmpassword == userpassword:
                 with open('users.txt', 'a') as f:
                     f.write(f'{useremail} {userpassword}\n')
                 print('» Cadastro efetuado com sucesso!')
+
+                nome = input("» Digite seu nome: ")
+                idade = input("» Digite sua idade: ")
+                telefone = input("» Digite seu telefone: ")
+                endereço = input("» Digite seu endereço: ")
+                print("Perfil criado com sucesso!")
+
+                return True
             else:
                 print("» As senhas não coincidem.")
+                return False
+
+### EDSON v
+
+def __init__(self, nome, idade, email,telefone,endereço):
+    self.nome = nome
+    self.idade = idade
+    self.email = email
+    self.telefone = telefone
+    self.endereço = endereço
+
+def exibir_perfil(self):    
+    print(f"Nome: {self.nome}")
+    print(f"Idade: {self.idade}")
+    print(f"email: {self.email}")
+    print(f"telefone: {self.telefone}")
+    print(f"endereço: {self.endereço}")
 
 # •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
@@ -41,22 +61,28 @@ def login():
                 email, pwd = line.split()
                 if userlogin == email and userpwd == pwd:
                     print("» Login efetuado com sucesso.")
-                    return
+                    return True
             print("» Senha incorreta.")
+            return False
         else:
             print("» Endereço de email não cadastrado.")
+            return False
 
 # •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
         # TELA DE INÍCIO - CADASTRO/LOGIN #
 
-escolhaentrada = int(input("» O que deseja fazer?\n1 → Cadastro\n2 → Login\n"))
-if escolhaentrada == 1:
-    cadastro()
-elif escolhaentrada == 2:
-    login()
-else:
-    print("» Opção inválida..")
+def tela_inicial():
+    while True:
+        escolhaentrada = int(input("» O que deseja fazer?\n1 → Cadastro\n2 → Login\n"))
+        if escolhaentrada == 1:
+            if cadastro():
+                break
+        elif escolhaentrada == 2:
+            if login():
+                break
+        else:
+            print("» Opção inválida.")
 
 # •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
@@ -88,17 +114,26 @@ def alimentacao():
                 break
             janta.append(comida)
         else:
-            print("» Opa, n ta pronto ainda")
+            print("Opa, n ta pronto ainda")
             break
 
 # •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
 
         # TELA PRINCIPAL #
 
-escolhaprincipal = int(input("» O que deseja fazer agora?\n1 → Controle da alimentação\n2 → etc\n"))
-if escolhaprincipal == 1:
-    alimentacao()
-else:
-    print("» Opa, n ta pronto ainda")
+def tela_principal():
+    while True:
+        escolhaprincipal = int(input("» O que deseja fazer agora?\n1 → Controle da alimentação\n2 → Exibir perfil\n"))
+        if escolhaprincipal == 1:
+            alimentacao()
+            break
+        elif escolhaprincipal == 2:
+            exibir_perfil()
+            break
+        else:
+            print("» Opção inválida.")
 
 # •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*
+
+tela_inicial()
+tela_principal()
